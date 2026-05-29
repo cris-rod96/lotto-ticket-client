1fa185225dbfbd733e7343cc722c7a166c443b1d
Cristhian Rodríguez <crisrodam1996@gmail.com>
2026-05-29 08:57:42 -0500

    Paginación de tickets desde el servidor

--- src/pages/admin/tickets/Tickets.jsx
+++ src/pages/admin/tickets/Tickets.jsx
@@ -99,6 +99,7 @@ const Tickets = () => {
       }
 
       const response = await ticketAPI.listarTodos(params)
+      console.log(response.data)
 
       // Asignamos los estados destructurados desde el nuevo JSON del servidor
       setTickets(response.data?.tickets || [])
