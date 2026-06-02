dc25e89c07e4fe6acdd51009a4bfc82dcf6cf9c1
Cristhian Rodríguez <crisrodam1996@gmail.com>
2026-06-02 01:19:43 -0500

    Fix de pago de tickets y actualización de tabla de ganadores

--- src/api/sorteo/sorteo.api.js
+++ src/api/sorteo/sorteo.api.js
@@ -2,9 +2,13 @@ import { instance } from '../base.api'
 const model = 'sorteos'
 
 const sorteoAPI = {
-  // Ahora recibe un objeto params (ej: { page: 1, limit: 5, estado: 'Abierto' })
   listarTodos: async (params = {}) => {
     return instance.get(`/${model}/listar/todos`, { params })
+  },
+
+  // AÑADE ESTO:
+  listarPorPunto: async (puntoVentaId, params = {}) => {
+    return instance.get(`/${model}/listar/por-punto/${puntoVentaId}`, { params })
   },
 
   crear: async (data) => {
@@ -15,12 +19,10 @@ const sorteoAPI = {
     return instance.patch(`/${model}/actualizar-sorteo/${id}`, data)
   },
 
-  // También añadimos params aquí por si necesitas paginar o filtrar los abiertos
   listarAbiertos: async (params = {}) => {
     return instance.get(`/${model}/listar/abiertos`, { params })
   },
 
-  // También añadimos params aquí por si necesitas paginar o filtrar los cerrados
   listarCerrados: async (params = {}) => {
     return instance.get(`/${model}/listar/cerrados`, { params })
   },
